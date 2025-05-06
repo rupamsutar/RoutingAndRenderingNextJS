e40ad86313cf36d0b311c7b42941b081b0bba679
rupamsutar <rupamsutar281998@gmail.com>
2025-05-06 20:20:15 +0530

    Handling the not found pages !

--- app/news/[newsId]/page.js
+++ app/news/[newsId]/page.js
@@ -1,8 +1,13 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
 
 export default function NewsDetails({params}) {
     const newsId = params.newsId;
     const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsId);
+
+    if (!newsItem) {
+        notFound();
+    }
     return (
         <article className="news-article">
             <header>
